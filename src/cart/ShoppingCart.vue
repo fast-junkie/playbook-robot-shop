05936319fe60ca1e97e42ac0bcf5f3bc05bdc6ff
Fast Junkie <fast-junkie@pm.me>
2024-11-29 14:33:31 -0500

    Completed module-o3...

--- src/cart/ShoppingCart.vue
+++ src/cart/ShoppingCart.vue
@@ -1,28 +1,48 @@
-<template>
-  <div class="container">
-    <h1>Your Cart</h1>
-    <EmptyCart v-if="cart.length === 0" />
-    <ul class="cart" v-if="cart.length > 0">
-      <li class="cart-item" v-for="(product, index) in cart" :key="index">
-        <ProductInfo :product="product">
-          <BaseButton @click="removeFromCart(product)">Remove</BaseButton>
-        </ProductInfo>
-      </li>
-    </ul>
-    <div v-if="cart.length > 0" class="total">Total: {{ toCurrency(cartTotal) }}</div>
-  </div>
-</template>
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <h1>Your Cart</h1>-->
+<!--    <EmptyCart v-if="cart.length === 0" />-->
+<!--    <ul class="cart" v-if="cart.length > 0">-->
+<!--      <li class="cart-item" v-for="(product, index) in cart" :key="index">-->
+<!--        <ProductInfo :product="product">-->
+<!--          <BaseButton @click="removeFromCart(product)">Remove</BaseButton>-->
+<!--        </ProductInfo>-->
+<!--      </li>-->
+<!--    </ul>-->
+<!--    <div v-if="cart.length > 0" class="total">Total: {{ toCurrency(cartTotal) }}</div>-->
+<!--  </div>-->
+<!--</template>-->
 
-<script setup>
+<script>
+import { h, resolveComponent } from 'vue'
 import { storeToRefs } from 'pinia'
 import EmptyCart from './EmptyCart.vue'
 import ProductInfo from '@/catalog/product-info/ProductInfo.vue'
 import { useCartStore } from '@/stores/cart'
 import { toCurrency } from '@/shared/formatters'
 
-const cartStore = useCartStore()
-const { cart, cartTotal } = storeToRefs(cartStore)
-const { removeFromCart } = cartStore
+export default {
+  render() {
+    const cartStore = useCartStore()
+    const { cart, cartTotal } = storeToRefs(cartStore)
+    const { removeFromCart } = cartStore
+    const BaseButton = resolveComponent('BaseButton')
+    const emptyCart = h(EmptyCart)
+    const cartItems = h('ul', { class: 'cart' },
+        cart.value.map((product, index) => h('li', { class: 'cart-item', key: index }, [
+          h(ProductInfo, { product }, () => h(BaseButton, { onClick: () => removeFromCart(product) }, () => 'Remove')),
+        ])),
+    )
+
+    return h('div', { class: 'container' }, [
+        h('h1', ['Your Cart']),
+        cart.value.length > 0 ? cartItems : emptyCart,
+        cart.value.length > 0 ? h('div', { class: 'total' }, [`Total: ${toCurrency(cartTotal.value)}`]) : null,
+    ])
+  },
+}
+
+
 
 </script>
 
@@ -38,7 +58,7 @@ const { removeFromCart } = cartStore
 
 .total {
   margin: 25px 175px 0 0;
-  align-self: right;
+  align-self: flex-end;
   font-size: 25px;
   text-align: right;
 }
